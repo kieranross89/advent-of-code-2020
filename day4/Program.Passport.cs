c5991051f599c1aa1d4d8946e09a83ba90ee882c
Kieran Ross <kieran.j.ross@gmail.com>
2020-12-13 21:39:28 +0000

    day 4 part 1

---
+++ day4/Program.Passport.cs
@@ -0,0 +1,22 @@
+namespace day4
+{
+    public class Passport
+    {
+        // Birth Year
+        public int byr { get; }
+        // Issue Year
+        public int iyr { get; }
+        // Expiration Year
+        public int eyr { get; }
+        // Height
+        public string hgt { get; }
+        // Hair Colour
+        public string hcl { get; }
+        // Eye colour
+        public string ecl {get;}
+        // Passport ID
+        public int pid {get;}
+        // Country ID 
+        public int cid {get;}
+    }
+}
---
+++ day4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day4
+{
+    partial class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var inputs = ParseInput(args[0]);
+
+            var result1 = new List<bool>();
+            foreach(var input in inputs)
+            {
+                result1.Add(Part1ValidatePassport(input));
+            }
+            
+            Console.WriteLine($"{result1.Where(x => x == true).Count()} passports are valid. {result1.Where(x => x == false).Count()} passports are invalid");
+        }
+
+        internal static IEnumerable<Dictionary<string, string>> ParseInput(string filename)
+        {
+             var contents = File.ReadAllText(filename);
+             var lookup = new Dictionary<string, string>();
+             // TODO this is nasty do better
+             return contents.Split("\r\n\r\n").Select(r => r.Replace("\r\n", " ").Split(' ').Select(x => x.Split(':')).ToDictionary(k => k[0], v => v[1]));
+        }
+
+        internal static bool Part1ValidatePassport(Dictionary<string, string> passport)
+        {
+            var requiredField = new string[] {"byr","iyr","eyr","hgt","hcl","ecl","pid"};
+            return requiredField.All(passport.ContainsKey);
+        }
+    }
+}
